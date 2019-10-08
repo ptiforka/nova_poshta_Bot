b04ad0bd082ee2aab1ec43334b9fdc1c87d73e6c
PtiforkaMetro <vladimir.tsiluiko@w.metroengines.jp>
2019-10-08 19:48:25 +0300

    integration with NP api

--- consts.py
+++ consts.py
@@ -5,3 +5,5 @@ load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 NP_TOKEN = os.getenv('NP_TOKEN')
+NP_URL = f'https://api.novaposhta.ua/v2.0/json'
+NP_TRACING_URL = f"{NP_URL}/getStatusDocuments"
