99ef8c8e9b374310f0304cacffb7de6ce5c8401e
ptiforka <35385354+ptiforka@users.noreply.github.com>
2019-10-19 09:38:59 +0300

    telegram integration with np (#3)

--- main.py
+++ main.py
@@ -1,13 +1,55 @@
-from consts import *
-
+from consts import TELEGRAM_TOKEN
 import telebot
+from np import NP
+from validation import *
 
 bot = telebot.TeleBot(TELEGRAM_TOKEN)
+np = NP()
 
 
-@bot.message_handler(commands='start')
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Yeah! basic setup completed!")
+    bot.reply_to(
+        message,
+        "Всем привет. Этот бот написан для экспресс-лекци по ботоводству которую мы проведем в СумГу")
+    send_available_commands(message)
+
+
+@bot.message_handler(commands=['help'])
+def send_available_commands(message):
+    bot.send_message(
+        message.chat.id, 'Для получения информации по ТТН просто отправьте нам её.'
+        ' Добавьте телефон для более подробной по посылке. Пример использования - ТТН НОМЕР_ТЕЛЕФОНА')
+
+
+@bot.message_handler()
+def find_ttn(message):
+    ttn, phone = unpack(message)
+    if ttn_validation(ttn) and phone_validation(phone):
+        bot.send_message(
+            message.chat.id,
+            serialize_message(
+                np.tracking(
+                    ttn,
+                    phone),
+                phone))
+    else:
+        bot.send_message(
+            message.chat.id,
+            'Неправильный ТТН или номер телефона')
+
+
+def serialize_message(np_response, phone):
+    message = f"""
+Ваша стоимость доставки - {np_response['delivery_cost']} гривны
+Вес посылки - {np_response['weight']} кг
+               """
+    if phone is not None:
+        message = f"{message} \nДоставка оформлена на имя - {np_response['recipient_name']}"
+    else:
+        message = f"{message} \nДля более подробный информации введите свой номер)"
+
+    return message
 
 
 if __name__ == '__main__':
--- np.py
+++ np.py
@@ -6,13 +6,20 @@ from requests import post
 # np.tracking('20450168245454', "0950830810")
 class NP:
     def tracking(self, document_number, phone=""):
-        data = self.serialize_data(
+        data = self.__serialize_data(
             'TrackingDocument',
             'getStatusDocuments',
-            self.tracking_resources(document_number, phone))
-        return self.send_request(NP_TRACING_URL, data).json()
+            self.__tracking_resources(document_number, phone))
+        return self.__send_request(NP_TRACING_URL, data)
 
-    def tracking_resources(self, document_number, phone):
+    def __serialize_answer(self, response):
+        return {
+            'delivery_cost': response['DocumentCost'],
+            'weight': response['CheckWeight'],
+            'recipient_name': response['RecipientFullName']
+        }
+
+    def __tracking_resources(self, document_number, phone):
         return {
             "methodProperties": {
                 "Documents": [
@@ -24,7 +31,7 @@ class NP:
             }
         }
 
-    def serialize_data(self, model_name, called_method, resources={}):
+    def __serialize_data(self, model_name, called_method, resources={}):
         return {
             'apiKey': NP_TOKEN,
             "modelName": model_name,
@@ -32,5 +39,8 @@ class NP:
             **resources
         }
 
-    def send_request(self, url, data):
-        return post(url, json=data)
+    def __send_request(self, url, data):
+        response = post(url, json=data).json()
+        return self.__serialize_answer(
+            response["data"][0]) if response["success"] is True else str(
+            response["errors"])
